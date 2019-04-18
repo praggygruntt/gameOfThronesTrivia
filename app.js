5f08b04e19f0a8d36a1ebef395b6a01ab7108660
Zachary Bowman <zchrybwmn@gmail.com>
2019-04-18 15:37:08 -0400

    Develop basic game start/stop functionality

--- app.js
+++ app.js
@@ -1,21 +1,31 @@
 // DOM ELEMENTS ========================================================
+
 const startReset    = document.getElementById("startReset");
 const scoreValue    = document.getElementById("score");
 const timer         = document.getElementById("timer");
+const timerValue    = document.getElementById("timerValue");
+const gameOver      = document.getElementById("gameOver");
+const correct       = document.getElementById("correct");
+const wrong         = document.getElementById("correct");
+
 
 // GLOBAL GAME VARIABLES ===============================================
+
 let playing = false;
 let score;
+let action;
+let timeRemaining;
 
 // GAME OPERATIONS =====================================================
-// Start/Reset Functionality
+
+// Start/Reset Game
 startReset.onclick = function() {
     // Reset Game
     if(playing === true) {
 
         // Reload page
         location.reload();
-        
+
     } 
         // Start Playing
         else {
@@ -33,5 +43,36 @@ startReset.onclick = function() {
 
             // Display timer
             timer.style.display = "block";
+            timeRemaining = 60;
+
+            // Start Countdown
+            startCountdown();
         }
 }
+
+// Start Countdown Function
+const startCountdown = () => {
+    // Set timer
+    action = setInterval(function(){
+        // reduce time by 1 sec
+        timeRemaining -= 1;
+        // update html
+        timerValue.innerHTML = timeRemaining;
+        // check if time is out
+        if(timeRemaining === 0) {
+            stopCountdown();
+            gameOver.style.visibility = "visible";
+            timer.style.display = "none";
+            correct.style.display = "none";
+            wrong.style.display = "none";
+            playing = false;
+        }
+    }, 100);
+};
+
+// Stop Countdown Function
+const stopCountdown = () => {
+    clearInterval(action);
+};
+
+// Generate Q&A! <-----START HERE ZACH!
